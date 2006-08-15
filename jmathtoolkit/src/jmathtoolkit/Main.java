140ad80d4a32058f09b40759e8bbd091f3278bcf
daspan <daspan>
2006-08-15 22:42:59 +0000

    Initial revision

---
+++ jmathtoolkit/src/jmathtoolkit/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on 16. August 2006, 00:40
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package jmathtoolkit;
+
+/**
+ *
+ * @author Linna
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
